f3e12baa1818b3d024292162e0050eb001b243ff
Dery Sudrajat <dery.sudrajat17@gmail.com>
2019-05-13 15:02:32 +0700

    Add Category Adapter and OptionItemMenu

--- app/src/main/java/com/example/mymovie/MainActivity.java
+++ app/src/main/java/com/example/mymovie/MainActivity.java
@@ -1,71 +1,40 @@
 package com.example.mymovie;
 
 import android.content.Intent;
-import android.content.res.TypedArray;
+import android.provider.Settings;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-    private String[] dataTitle;
-    private String[] dataYear;
-    private String[] dataRelease;
-    private String[] dataRuntime;
-    private String[] dataGenre;
-    private String[] dataCast;
-    private String[] dataOverview;
-    private TypedArray dataPoster;
-    private MovieAdapter adapter;
-    private ArrayList<Movie> movies;
-    public static final String MOVIE_EXTRA = "movie_extra";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView listView = findViewById(R.id.lv_list);
-        adapter = new MovieAdapter(this);
-        listView.setAdapter(adapter);
-        prepare();
-        addItem();
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(MainActivity.this, MovieDetail.class);
-                intent.putExtra(MOVIE_EXTRA, movies.get(position));
-                startActivity(intent);
-                Toast.makeText(MainActivity.this, movies.get(position).getTitle(), Toast.LENGTH_SHORT).show();
-            }
-        });
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
-    private void addItem() {
-        movies = new ArrayList<>();
-        for (int i = 0; i < dataTitle.length; i++) {
-            Movie movie = new Movie(
-                    dataPoster.getResourceId(i, -1),
-                    dataTitle[i], dataYear[i], dataRelease[i],
-                    dataRuntime[i], dataGenre[i], dataCast[i],
-                    dataOverview[i]);
-            movies.add(movie);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_change_settings) {
+            Intent mIntent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(mIntent);
         }
-        adapter.setMovies(movies);
+        return super.onOptionsItemSelected(item);
     }
-
-    private void prepare() {
-        dataTitle = getResources().getStringArray(R.array.movie_title);
-        dataYear = getResources().getStringArray(R.array.movie_year);
-        dataRelease = getResources().getStringArray(R.array.release_info);
-        dataRuntime = getResources().getStringArray(R.array.movie_runtime);
-        dataGenre = getResources().getStringArray(R.array.movie_genre);
-        dataCast = getResources().getStringArray(R.array.movie_cast);
-        dataOverview = getResources().getStringArray(R.array.movie_overview);
-        dataPoster = getResources().obtainTypedArray(R.array.movie_poster);
-    }
-
 }
