78672d31af527b222a53cb2ae04eaf674681547d
derysudrajat <dery.sudrajat17@gmail.com>
2019-05-25 08:56:54 +0700

    Add component mainViewModel to load data

--- app/src/main/java/com/example/mymovie/MoviesFragment.java
+++ app/src/main/java/com/example/mymovie/MoviesFragment.java
@@ -1,15 +1,20 @@
 package com.example.mymovie;
 
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import java.util.ArrayList;
 
@@ -17,61 +22,73 @@ import java.util.ArrayList;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MoviesFragment extends Fragment {
-    private String[] dataTitle;
-    private String[] dataYear;
-    private String[] dataRelease;
-    private String[] dataRuntime;
-    private String[] dataGenre;
-    private String[] dataCast;
-    private String[] dataOverview;
-    private TypedArray dataPoster;
-    private MoviesAdapter adapter;
-    public static final String MOVIE_EXTRA = "movie_extra";
+public class MoviesFragment extends Fragment{
 
+    private MoviesAdapter adapter;
+    private ProgressBar progressBar;
+    private MainViewModel mainViewModel;
+    public static final String MOVIE_EXTRA = "movie_extra";
+    public static final String MOVIE_KEY = "movie";
+    public String Language, Hours, Munites, As;
+    String[] AdditionalState = new String[4];
 
     public MoviesFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.list_item, container, false);
-        RecyclerView rvMovies = rootView.findViewById(R.id.mainRv);
-        rvMovies.setHasFixedSize(true);
-        rvMovies.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        progressBar = rootView.findViewById(R.id.progressBar);
+        Language = getResources().getString(R.string.lang);
+        Hours = getResources().getString(R.string.hours);
+        Munites = getResources().getString(R.string.munites);
+        As = getResources().getString(R.string.as);
+        AdditionalState[0] = Language;
+        AdditionalState[1] = As;
+        AdditionalState[2] = Hours;
+        AdditionalState[3] = Munites;
+        mainViewModel = ViewModelProviders.of(getActivity()).get(MainViewModel.class);
+        mainViewModel.getMovies().observe(getActivity(),getMovie);
+
         adapter = new MoviesAdapter(getActivity());
-        prepare();
-        addItem();
+        adapter.notifyDataSetChanged();
+
+        RecyclerView rvMovies = rootView.findViewById(R.id.mainRv);
+        rvMovies.setLayoutManager(new LinearLayoutManager(getActivity()));
         rvMovies.setAdapter(adapter);
+
+        showData();
+
         return rootView;
 
     }
 
-    private void addItem() {
-        ArrayList<Movie> movies = new ArrayList<>();
-        for (int i = 0; i < dataTitle.length; i++) {
-            Movie movie = new Movie(
-                    dataPoster.getResourceId(i, -1),
-                    dataTitle[i], dataYear[i], dataRelease[i],
-                    dataRuntime[i], dataGenre[i], dataCast[i],
-                    dataOverview[i]);
-            movies.add(movie);
+    private  Observer<ArrayList<Movie>> getMovie = new Observer<ArrayList<Movie>>() {
+        @Override
+        public void onChanged(@Nullable ArrayList<Movie> movieItems) {
+            if (movieItems!=null){
+                adapter.setmData(movieItems);
+                showLoading(false);
+            }
         }
-        adapter.setListMovie(movies);
+    };
+
+    private void showData(){
+        mainViewModel.setData(MOVIE_KEY,AdditionalState);
+        showLoading(true);
     }
 
-    private void prepare() {
-        dataTitle = getResources().getStringArray(R.array.movie_title);
-        dataYear = getResources().getStringArray(R.array.movie_year);
-        dataRelease = getResources().getStringArray(R.array.release_info);
-        dataRuntime = getResources().getStringArray(R.array.movie_runtime);
-        dataGenre = getResources().getStringArray(R.array.movie_genre);
-        dataCast = getResources().getStringArray(R.array.movie_cast);
-        dataOverview = getResources().getStringArray(R.array.movie_overview);
-        dataPoster = getResources().obtainTypedArray(R.array.movie_poster);
+    private void showLoading(Boolean state) {
+        if (state) {
+            progressBar.setVisibility(View.VISIBLE);
+        } else {
+            progressBar.setVisibility(View.GONE);
+        }
     }
+
+
 
 }
